cfee7bde17138dc60885773211af20cc76db6b5e
ElihuJones <xelijone@amazon.com>
2022-09-08 14:03:10 -0400

    Fizz-Buzz using enum.
    Not the actual lab.

--- src/main/java/com/tlglearning/Main.java
+++ src/main/java/com/tlglearning/Main.java
@@ -1,11 +1,22 @@
 package com.tlglearning;
 
+import java.sql.SQLOutput;
+import java.util.Set;
+
 public class Main {
 
   public static void main(String[] args) {
-    for (int i = 0; i <= 100; i++) {
-      System.out.println(FizzBuzz.valueOf(i));
-    }
+    //for(Initialize, Test (boolean expression), Update))
+    //for(int i = 0; i <= 100; i++)  {
+    //   System.out.println(FizzBuzz.valueOf(i));
+
+    //do - while(Test, Statement)
+    int i = 1;
+    do {
+      Set<FizzBuzz> value = FizzBuzz.valueOf(i);
+      System.out.printf("%d: %s%n", i, value);
+      i++;
+    } while (i <= 100);
   }
 
 }
